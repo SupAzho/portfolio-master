b885a4fc4792b7896d78e6f3ca3780f1290e60b5
SupAazho <sapnaafsar86@gmail.com>
2023-06-07 18:22:03 -1000

    add more data

---
+++ portfolio.js
@@ -0,0 +1,321 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "SupAzho",
+	title: "Hi all, I'm Sapna",
+	description:
+		"I'm passionate Front End web developer having an experience of web applications with React.js, Next.js, HTML, CSS(Bootstrap,Material UI), JS, Web3.Blockchain and Brownie Framework & WwordPress.",
+	
+		// 
+};
+
+export const openSource = {
+	githubUserName: "SupAzho",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	linkedin: "https://www.linkedin.com/in/sapna-%E2%99%8C-bb003121b",
+	github: "https://github.com/SupAzho",
+	twitter: "https://twitter.com/SupAzho?t=ZYeEmTTn6z8-feGA5LkMWA&s=09",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FRONT END DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	data: [
+		{
+			title: "Front End Development",
+			lottieAnimationFile: "/lottie/skills/frontEnd.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji(
+					"⚡ Building responsive Single-Page-Apps (SPA) & PWA in Next.js"
+				),
+				emoji("⚡ Building responsive static websites using React.js"),
+				
+			],
+			softwareSkills: [
+				{
+					skillName: "HTML-5",
+					fontAwesomeClassname: "vscode-icons:file-type-html",
+				},
+				{
+					skillName: "CSS-3",
+					fontAwesomeClassname: "vscode-icons:file-type-css",
+				},
+				{
+					skillName: "JavaScript",
+					fontAwesomeClassname: "logos:javascript",
+				},
+				{
+					skillName: "Reactjs",
+					fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+				},
+	   			
+		{
+					skillName: "Redux",
+					fontAwesomeClassname: "logos:redux",
+				},
+				{
+					skillName: "NPM",
+					fontAwesomeClassname: "logos:npm-icon",
+				},
+				{
+					skillName: "Yarn",
+					fontAwesomeClassname: "logos:yarn",
+				},
+			],
+		},
+		{
+			title: "Cloud Infra-Architecture",
+			lottieAnimationFile: "/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji("⚡ Experience of working on multiple cloud platforms"),
+				emoji(
+					"⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
+				),
+				emoji(
+					"⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions"
+				),
+			],
+			softwareSkills: [
+				// ? Check README To get icon details
+				{
+					skillName: "AWS",
+					fontAwesomeClassname: "logos:aws",
+				},
+				{
+					skillName: "Azure",
+					fontAwesomeClassname: "logos:microsoft-azure",
+				},
+				{
+					skillName: "Heroku",
+					fontAwesomeClassname: "logos:heroku-icon",
+				},
+				{
+					skillName: "PostgreSQL",
+					fontAwesomeClassname: "logos:postgresql",
+				},
+				{
+					skillName: "Github",
+					fontAwesomeClassname: "akar-icons:github-fill",
+				},
+				{
+					skillName: "Docker",
+					fontAwesomeClassname: "logos:docker-icon",
+				},
+				{
+					skillName: "Github Actions",
+					fontAwesomeClassname: "logos:github-actions",
+				},
+				{
+					skillName: "Cloudinary",
+					fontAwesomeClassname: "logos:cloudinary",
+				},
+				{
+					skillName: "Nginx",
+					fontAwesomeClassname: "logos:nginx",
+				},
+				{
+					skillName: "Sentry",
+					fontAwesomeClassname: "logos:sentry-icon",
+				},
+			],
+		},
+		{
+			title: "Blockchain",
+			lottieAnimationFile: "/lottie/skills/ethereum.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji(
+					"⚡ Experience in developing Smart Contract using Solidity & Ethereum"
+				),
+				emoji(
+					"⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura"
+				),
+				emoji(
+					"⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles"
+				),
+				emoji(
+					"⚡ Developing NFT Smart Contracts using ERC-721 Token Standard"
+				),
+				emoji(
+					"⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS"
+				),
+			
+						],
+			softwareSkills: [
+				{
+					skillName: "Ethereum",
+					fontAwesomeClassname: "logos:ethereum",
+				},
+				{
+					skillName: "Solidity",
+					fontAwesomeClassname: "logos:solidity",
+				},
+				{
+					skillName: "Web3js",
+					fontAwesomeClassname: "logos:web3js",
+				},
+				{
+					skillName: "Metamask",
+					fontAwesomeClassname: "logos:metamask-icon",
+				},
+				{
+					skillName: "Ganache",
+					fontAwesomeClassname: "logos:ganache-icon",
+				},
+			],
+		},
+		
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "20",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Abdul Wali Khan University",
+		subHeader: "Master of Science in Computer Science",
+		duration: "September 2017 - April 2021",
+		// desc: "Participated in the research of XXX and published 3 papers.",
+		grade: "Grade A",
+		descBullets: [
+			
+		],
+	},
+];
+
+export const experience = [
+	{
+		role: "Frontend Developer",
+		company: "IT Software Company",
+		companylogo: "/img/icons/common/IT.jpg",
+		date: "Apr 2018 – Jun 2019",
+		desc: "I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries. Responsible for Use markup languages like HTML to create user-friendly web pages.Maintain and improve website Optimize applications for maximum speed.Design mobile-based features .Collaborate with back-end developers and web designers to improve usability.",
+	},
+	{
+		role: "WordPress Developer",
+		company: "Tekrowe Software",
+		companylogo: "/img/icons/common/Tekrowe.jpg",
+		date: "Jan 2019 – Mar 2020",
+		desc: "I worked as wordpress developer,Responsible for both back-end and front-end development including creating WordPress themes and plugins.Also responsible for Designing and implementing new features and functionality Establishing and guiding the website's architecture.Ensuring high-performance and availability and managing all technical aspects of the CMS Helping formulate an effective, responsive design and turning it into a working theme and plugin.",
+		
+	},
+	
+	{
+		role: "Backend Developer",
+		company: "Microsoft",
+		companylogo: "/img/icons/common/Microsoft.jpg",
+		date: "Sept 2020-jan 2021",
+		desc: "I Worked as a backend Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.Build and maintain websitesTroubleshoot and debug.Write high-quality code.",
+	},
+	{
+		role: "Front End Developer",
+		company: "Cloudtek",
+		companylogo: "/img/icons/common/Cloudtek.jpg",
+		date: "Sept 2021 - continue",
+		desc: "Worked as cloudtek web developer to create a website in which I integrated computer vision AI model to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.",
+	},
+];
+
+export const projects = [
+	{
+		name: " Technical portfolio website",
+		desc: "Front-End ",
+		
+	},
+	{
+		name: "Landing page",
+		desc: "Front-End",
+	
+	},
+	{
+		name: "Responsive virtual keyboard",
+		desc: "Front-End ",
+		
+	},
+	{
+		name: "E-Commerce Website",
+		desc: "Front-End , Back-End",
+		
+	},
+	{
+		name: "A clone website",
+		desc: "Front-End , Back-End",
+		
+	},
+	{
+		name: "A 'perfect score' website",
+		desc: "Front-End , Back-End",
+		
+	},
+	{
+		name: "A 'perfect score' website",
+		desc: " wordpress ",
+		
+	},
+	{
+		name: " Online Courses",
+		desc: " wordpress ",
+		
+	},
+	{
+		name: " Travel Website",
+		desc: " Front-End ",
+		
+	},
+	{
+		name: "Fashion / Lifestyle Website",
+		desc: " wordpress ",
+		
+	},
+	{
+		name: " Social Network Website",
+		desc: " wordpress ",
+		
+	},
+	{
+		name: "Online Forums Website",
+		desc: " Front-End ",
+		
+	},
+];
+
+
+
+// option to hide or show the ContactUs component
+export const showContactUs = true;
+
+// See object prototype on SEO.jsx page
+export const seoData = {
+	title: "Sapna Afsar",
+	description:
+		"A passionate Front End Web Developer and Blockchain Developer.",
+	author: "Sapna",
+	image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+	
+	keywords: [
+		"Sapna",
+		"Sapna Afsar",
+		"@SupAzho",
+		"SupAzho",
+		"Portfolio",
+		"Sapna Portfolio ",
+		// "Sapna Afsar Portfolio",
+	],
+}
